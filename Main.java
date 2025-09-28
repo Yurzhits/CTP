0cba6864b5e7ff19b1ed4d837bc507e350b1f31f
Maria <m_yurzhits@mail.ru>
2025-09-28 22:34:10 +0300

    Second commit

--- Main.java
+++ Main.java
@@ -4,5 +4,8 @@ public class Main {
 
         Person student = new Person("Maria", 20);
         System.out.println(student);
+
+        Person teacher = new Person("Ekaterina", 25);
+        System.out.println(teacher);
     }
 }
