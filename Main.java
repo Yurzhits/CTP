97ce8bfc163c596cee4e4aa0de1e53ddd5922722
Maria <m_yurzhits@mail.ru>
2025-09-28 22:31:45 +0300

    First commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        Person student = new Person("Maria", 20);
+        System.out.println(student);
+    }
+}
